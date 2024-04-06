c4e49a2f764e926f8000f0b96f178199f457a39a
elythh <gwen@omg.lol>
2024-04-06 15:19:54 +0200

    wip: test

--- config/plug/ui/alpha.nix
+++ config/plug/ui/alpha.nix
@@ -19,70 +19,13 @@
     enable = true;
     layout = [
       {
-        type = "padding";
-        val = 4;
-      }
-      {
+        type = "terminal";
+        command = "pfetch";
+        width = 46;
+        height = 25;
         opts = {
-          hl = "AlphaHeader";
-          position = "center";
+          redraw = true;
         };
-        type = "text";
-        val = nixFlake;
-      }
-      {
-        type = "padding";
-        val = 2;
-      }
-      {
-        type = "group";
-        val = let
-          mkButton = shortcut: cmd: val: hl: {
-            type = "button";
-            inherit val;
-            opts = {
-              inherit hl shortcut;
-              keymap = [
-                "n"
-                shortcut
-                cmd
-                {}
-              ];
-              position = "center";
-              cursor = 0;
-              width = 40;
-              align_shortcut = "right";
-              hl_shortcut = "Keyword";
-            };
-          };
-        in [
-          (
-            mkButton
-            "f"
-            "<CMD>lua require('telescope.builtin').find_files({hidden = true})<CR>"
-            "🔍 Find File"
-            "Operator"
-          )
-          (
-            mkButton
-            "q"
-            "<CMD>qa<CR>"
-            "💣 Quit Neovim"
-            "String"
-          )
-        ];
-      }
-      {
-        type = "padding";
-        val = 2;
-      }
-      {
-        opts = {
-          hl = "GruvboxBlue";
-          position = "center";
-        };
-        type = "text";
-        val = "https://github.com/elythh/nixvim";
       }
     ];
   };
